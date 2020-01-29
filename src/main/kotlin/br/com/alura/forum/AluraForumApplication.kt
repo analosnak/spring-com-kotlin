5293896995cbb00352cb12badfe493bae03cbea7
Ana Losnak <ana.losnak@caelum.com.br>
2020-01-29 18:33:25 -0300

    Projeto inicial do Spring Initializr

---
+++ src/main/kotlin/br/com/alura/forum/AluraForumApplication.kt
@@ -0,0 +1,11 @@
+package br.com.alura.forum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraForumApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraForumApplication>(*args)
+}
