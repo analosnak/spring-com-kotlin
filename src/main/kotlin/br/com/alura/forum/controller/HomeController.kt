454a4029abd6bf2057777872a4ba915f1b1b03fe
Ana Losnak <ana.losnak@caelum.com.br>
2020-01-29 21:05:07 -0300

    Fazendo HomeController funcionar

--- src/main/kotlin/br/com/alura/forum/controller/HomeController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/HomeController.kt
@@ -2,10 +2,12 @@ package br.com.alura.forum.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HomeController {
     @RequestMapping("/")
+    @ResponseBody
     fun index(): String {
         print("Log do servidor de que foi feita uma requisição para '/'.")
         return "Bem vindo ao fórum da Alura"
