554cd8e188c691f749459563ddcc8988982f5042
Ana Losnak <ana.losnak@caelum.com.br>
2020-01-31 16:59:30 -0300

    Adicionando ordenação

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -5,6 +5,8 @@ import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto
 import br.com.alura.forum.repository.TopicRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TopicController(private val topicRepository: TopicRepository) {
     @GetMapping("/api/topics", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun listTopics(topicSearch: TopicSearchInputDto, pageRequest: Pageable): Page<TopicBriefOutputDto> =
+    fun listTopics(
+        topicSearch: TopicSearchInputDto,
+        @PageableDefault(sort = ["creationInstant"], direction = Sort.Direction.DESC) pageRequest: Pageable
+    ): Page<TopicBriefOutputDto> =
         topicRepository.findAll(topicSearch.toSpecification(), pageRequest)
             .map { TopicBriefOutputDto(it) }
 }
