55cc936283e51c1b008bed6d87ee2952ffb93bb1
Ana Losnak <ana.losnak@caelum.com.br>
2020-01-31 16:54:43 -0300

    Colocando a paginação

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.controller.dto.input.TopicSearchInputDto
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto
 import br.com.alura.forum.repository.TopicRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TopicController(private val topicRepository: TopicRepository) {
     @GetMapping("/api/topics", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun listTopics(topicSearch: TopicSearchInputDto): List<TopicBriefOutputDto> =
-        topicRepository.findAll(topicSearch.toSpecification()).map { TopicBriefOutputDto(it) }
+    fun listTopics(topicSearch: TopicSearchInputDto, pageRequest: Pageable): Page<TopicBriefOutputDto> =
+        topicRepository.findAll(topicSearch.toSpecification(), pageRequest)
+            .map { TopicBriefOutputDto(it) }
 }
